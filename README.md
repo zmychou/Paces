94d53bc58106512a01993de6b97716e1569f9f3e
zmychou <zmychou@gmail.com>
2017-05-11 22:50:21 +0800

    add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# Paces
+
+This is graduate project.  
+It is a running app which can record the data such as longitude, latitude, distance, duration and speed etc.  
+
+It also include a pedometer and a music playback.
+
+The repository **Paces_server** is the server side of it.
---
+++ app/src/main/java/com/zmychou/paces/network/DownloadRecordRequests.java
@@ -0,0 +1,23 @@
+package com.zmychou.paces.network;
+
+import java.io.InputStream;
+
+/**
+ * <pre>
+ * Package    :com.zmychou.paces.network
+ * Author     : zmychou
+ * Create time:17-5-7
+ * Last Modify:
+ * Email      :zmychou@gmail.com
+ * Version    :V1.0
+ * description:
+ * </pre>
+ */
+
+public class DownloadRecordRequests extends Requests {
+
+    @Override
+    protected void onPostExecute(InputStream inputStream) {
+
+    }
+}
